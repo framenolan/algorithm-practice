507d47716d650eb3753da8ef4416d7b4115976f3
Nolan Frame <frame.nolan@gmail.com>
2022-10-13 09:14:19 -0700

    number 2 complete

--- Week 5/02-is-unique/Unsolved/is-unique.js
+++ Week 5/02-is-unique/Unsolved/is-unique.js
@@ -2,6 +2,23 @@
 // Return `true` is no number appears in the array more than once, else return `false`
 
 var isUnique = function(arr) {
-    
-
+    if(arr.length === 0) {
+        return true;
+    } else {        
+        
+        for(i=0; i<arr.length; i++) {
+            let checkVal = arr[i];
+            if (i === (arr.length-1)) {
+                return true;
+            } else {
+                for(let j = (i+1); j<arr.length; j++) {
+                    if(arr[j] === checkVal) {
+                        return false;
+                    } else if (i === arr.length) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
 };
