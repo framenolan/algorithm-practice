ed66bbb3e6b3f4869706c4da3471f5222ad89ca6
Nolan Frame <frame.nolan@gmail.com>
2022-10-13 10:42:46 -0700

    taking break, try again later

--- Week 5/03-is-anagram/Unsolved/is-anagram.js
+++ Week 5/03-is-anagram/Unsolved/is-anagram.js
@@ -2,26 +2,44 @@
 // Return true if the strings are anagrams of each other, otherwise return false
 
 var isAnagram = function(strA, strB) {
+    // This is not working, maybe start from scratch
+    
     const arrA = strA.split("");
-    const arrB = [];
-    console.log(arrA)
-    console.log(arrB)
+    const arrB = strB.split("");
+    let arrC = arrA[0].splice(0, 1);
+    console.log("A",arrA)
+    console.log("B",arrB)
+    console.log("C",arrC)
 
-    for(i=arrA.length-1; i>=0; i--) {
-        arrB.push(arrA[i])
-
-        console.log("arrA i",arrA[i])
-        console.log("arrB",arrB)
+    function compare(array1, array2) {
+        for(i=0; i<array1.length; i++) {
+            console.log("for1")
+            for(j=0; j<array2.length; j++) {
+                console.log("for2")
+                console.log("i",i,"j",j)
+                if(array1[i] === array2[j]) {
+                    console.log("i",i,"j",j)
+                    console.log(arrA)
+                    console.log(arrB)
+                    console.log(array1[i].splice(i, 1));
+                    array1[i].splice(i, 1);
+                    console.log(array1)
+                    array2[j].splice(j,1);
+                    console.log(array2)
+                } else {
+                    console.log("else2")
+                }
+            }
+        }
     }
 
-    const strC = arrB.join("");
-    console.log("strC",strC)
-    console.log("strB",strB)
-
-    if(strC === strB) {
-        return true;
-    } else {
+    if(arrA.length !== arrB.length) {
         return false;
+    } else {
+        console.log("else")
+        compare(arrA, arrB);
+        console.log("A",arrA)
+        console.log("B",arrB)
     }
 
 };
