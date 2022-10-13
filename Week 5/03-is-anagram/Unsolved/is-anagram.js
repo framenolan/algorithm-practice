264a865d1843e0cddcd8c830cd627b2f2b1f8684
Nolan Frame <frame.nolan@gmail.com>
2022-10-13 09:39:31 -0700

    number 3, checked if palindrome rather than anagram

--- Week 5/03-is-anagram/Unsolved/is-anagram.js
+++ Week 5/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,27 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+    const arrA = strA.split("");
+    const arrB = [];
+    console.log(arrA)
+    console.log(arrB)
+
+    for(i=arrA.length-1; i>=0; i--) {
+        arrB.push(arrA[i])
+
+        console.log("arrA i",arrA[i])
+        console.log("arrB",arrB)
+    }
+
+    const strC = arrB.join("");
+    console.log("strC",strC)
+    console.log("strB",strB)
+
+    if(strC === strB) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
