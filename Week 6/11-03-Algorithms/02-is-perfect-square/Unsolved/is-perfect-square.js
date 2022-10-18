5489cd0f7346c0c6e166620e34c1c2f2572d7eb2
Nolan Frame <frame.nolan@gmail.com>
2022-10-18 09:28:07 -0700

    week 6.11 number 2 completed

--- Week 6/11-03-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
+++ Week 6/11-03-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,21 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+var isPerfectSquare = function(num) {
+
+    if(num === 0) {
+        return true;
+    } else if(num < 0) {
+        return false;
+    } else {
+        for(i=1; i<=num; i++) {
+            let rootTest = num / i;
+            
+            if(rootTest === i) {
+                return true;
+            } else if(i === num) {
+                return false;
+            }
+        };   
+    }
+}
