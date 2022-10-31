23fc505508a18086441217858c438f3a6908075e
Nolan Frame <frame.nolan@gmail.com>
2022-10-31 13:40:50 -0700

    completed 2d array search

--- Week 6/11-03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ Week 6/11-03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -3,6 +3,18 @@
 // Return the number of times "X" appears in the 2D array
 
 var arraySearch2D = function(arr) {
+    let count = 0;
 
+    for(i=0; i<arr.length; i++) {
+
+        let innerLength = arr[i].length;
+
+        for(j=0; j<innerLength; j++) {
+            if(arr[i][j] === "X") {
+                count = count +1;
+            }
+        }
+    }
     
+    return count;
 };
