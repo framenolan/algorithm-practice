245d9d91d08c42bd36dbe6d88b7ab9ff2bb01393
Nolan Frame <frame.nolan@gmail.com>
2022-10-31 13:22:04 -0700

    week 6 12-1 finished

--- Week 6/12-03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
+++ Week 6/12-03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,3 +1,20 @@
 const multiplyInto20 = function(arr) {
   // TODO: Write Your Code Here
+
+  
+  for(i=0; i<arr.length-1; i++) {
+
+    for(j=(i+1); j<arr.length; j++) {
+
+      if(arr[i] * arr[j] === 20) {
+        return true;
+      }
+
+    }
+    
+  }
+
+  return false;
+
 };
+
