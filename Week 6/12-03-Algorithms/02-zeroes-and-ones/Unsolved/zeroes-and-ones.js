ac51b0413e6f63d981842b942858f1ac51fed961
Nolan Frame <frame.nolan@gmail.com>
2022-10-31 13:48:32 -0700

    zeroes and ones completed

--- Week 6/12-03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ Week 6/12-03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,23 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function(str) {
+    let count0 = 0;
+    let count1 = 0;
+
+    for(i=0; i<str.length; i++) {
+        
+        if(str[i] === "0") {
+            count0 = count0 +1;
+        } else if (str[i] === "1") {
+            count1 = count1 +1;
+        }
+    }
+
+    if(count0 === count1) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
